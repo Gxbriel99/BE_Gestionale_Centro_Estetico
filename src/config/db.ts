0714b14030a6762a937f78fbf4d3411b199cc348
Gabriel <gabriel.perini99@gmail.com>
2025-09-28 19:08:21 +0200

    Implementazizone file .env
    Modifica della struttura di collegamento al DB no SQL su atlas

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://gabriel:G7s9KqX_@gestionale.qyhqyv8.mongodb.net/?retryWrites=true&w=majority&appName=Gestionale",
-            { dbName: "Gestionale" }
+            process.env.MONGO_URI!,
+            { dbName: process.env.DB_NAME }
         );
-
-        //{ dbName: "Gestionale" } specifica il nome del database da utilizzare
 
         console.log("MongoDB Atlas connesso!");
     } catch (err) {
